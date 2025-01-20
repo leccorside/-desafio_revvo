0fed4828ad86b3c9fb83d701c8bfeaf9f92d1882
Johnathan <leccorside@gmail.com>
2025-01-20 14:43:09 -0300

    Correção Model

--- Models/Course.php
+++ Models/Course.php
@@ -3,11 +3,7 @@
 class Course {
     public static function getAllCourses() {
         $data = file_get_contents('data/courses.json');
-<<<<<<< HEAD
         $courses = json_decode($data, true);
         return is_array($courses) ? $courses : [];
-=======
-        return json_decode($data, true);
->>>>>>> 9ab2e4cba9df0870554b2d662d586f795552c07b
     }
 }
